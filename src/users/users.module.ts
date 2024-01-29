a121ec7c609cd8ed457915a148542335c2ac51ff
Guilherme Afonso <profguilherme.as@gmail.com>
2024-01-29 13:29:56 -0300

    feat: setting user service

--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -1,6 +1,7 @@
 import { Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
 import { User, UserSchema } from 'src/schemas/User.schema';
+import { UsersService } from './users.service';
 
 @Module({
   imports: [
@@ -11,5 +12,6 @@ import { User, UserSchema } from 'src/schemas/User.schema';
       },
     ]),
   ],
+  providers: [UsersService],
 })
 export class UsersModule {}
