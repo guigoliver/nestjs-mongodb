35edf1c31e4e8c7fc90d69e0738944a085cecba4
Guilherme Afonso <profguilherme.as@gmail.com>
2024-01-29 11:37:59 -0300

    feat: minor adjustments

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,12 @@
 import { Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
+import { UsersModule } from './users/users.module';
 
 // eslint-disable-next-line @typescript-eslint/no-var-requires
 require('dotenv').config();
 
 @Module({
-  imports: [MongooseModule.forRoot(process.env.MONGO_URI)],
+  imports: [MongooseModule.forRoot(process.env.MONGO_URI), UsersModule],
   controllers: [],
   providers: [],
 })
