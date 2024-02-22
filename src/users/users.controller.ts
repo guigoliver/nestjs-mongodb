2e433c8d4bac5b425bfdffdb8a603e20a379b24c
Guilherme Afonso <profguilherme.as@gmail.com>
2024-02-22 10:22:43 -0300

    feat: get user by id

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,6 +1,14 @@
-import { Body, Controller, Get, Post } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Get,
+  HttpException,
+  Param,
+  Post,
+} from '@nestjs/common';
 import { UsersService } from './users.service';
 import { CreateUserDto } from './dto/CreateUser.dto';
+import mongoose from 'mongoose';
 
 @Controller('users')
 export class UsersController {
@@ -13,5 +21,14 @@ export class UsersController {
   @Get()
   getUsers() {
     return this.usersService.getUsers();
+  }
+
+  @Get(':id')
+  async getUserById(@Param('id') id: string) {
+    const isIdValid = mongoose.Types.ObjectId.isValid(id);
+    if (!isIdValid) throw new HttpException('User not found', 404);
+    const findUser = await this.usersService.getUserById(id);
+    if (!findUser) throw new HttpException('User not found', 404);
+    return findUser;
   }
 }
