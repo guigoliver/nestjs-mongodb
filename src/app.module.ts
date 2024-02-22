2e770bc0b383c24adfe25ae161473ba5927ceda3
Guilherme Afonso <profguilherme.as@gmail.com>
2024-02-22 08:53:35 -0300

    feat: add post and get user actions

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,10 +3,17 @@ import { MongooseModule } from '@nestjs/mongoose';
 import { UsersModule } from './users/users.module';
 
 // eslint-disable-next-line @typescript-eslint/no-var-requires
-require('dotenv').config();
+const dotenv = require('dotenv');
+dotenv.config();
+// const url = process.env.MONGO_URI;
 
 @Module({
-  imports: [MongooseModule.forRoot(process.env.MONGO_URI), UsersModule],
+  imports: [
+    MongooseModule.forRoot(
+      'mongodb+srv://profguilhermeas:justoserafim@scrum.hpte78n.mongodb.net/?retryWrites=true&w=majority',
+    ),
+    UsersModule,
+  ],
   controllers: [],
   providers: [],
 })
---
+++ src/users/users.controller.ts
@@ -0,0 +1,17 @@
+import { Body, Controller, Get, Post } from '@nestjs/common';
+import { UsersService } from './users.service';
+import { CreateUserDto } from './dto/CreateUser.dto';
+
+@Controller('users')
+export class UsersController {
+  constructor(private usersService: UsersService) {}
+  @Post()
+  createUser(@Body() createUserDto: CreateUserDto) {
+    return this.usersService.createUser(createUserDto);
+  }
+
+  @Get()
+  getUsers() {
+    return this.usersService.getUsers();
+  }
+}
